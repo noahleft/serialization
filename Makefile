157a6ea2125841fd568d081bc1d366d25d9222e0
ShihFengLin <noahleft@gmail.com>
2021-06-13 17:08:08 +0800

    add example case on protobuf to serialize the bus_stop

--- Makefile
+++ Makefile
@@ -1,6 +1,13 @@
 
-prog: *.cpp */*
-	g++ -Idata -Iboost_serialization main.cpp -o $@ -lboost_serialization
+all: exec_boost exec_protobuf
+
+exec_boost: *.cpp data/* boost_serialization/*
+	g++ -Idata -Iboost_serialization -DBOOST_EXAMPLE main.cpp -o $@ -lboost_serialization
+
+exec_protobuf: *.cpp data/* protobuf/*
+	protoc -I=protobuf --cpp_out=protobuf protobuf/addressbook.proto
+	g++ -Idata -Iprotobuf -DPROTOBUF_EXAMPLE -std=c++11 main.cpp protobuf/*.cc -o $@ -lprotobuf
 
 clean:
-	rm prog
+	rm exec_* -f
+	rm protobuf/*.h protobuf/*.cc
--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-#include "bus_stop.hpp"
-#include "bus_stop_ser.hpp"
 #include <sstream>
+#include "bus_stop.hpp"
+
+#ifdef BOOST_EXAMPLE
+#include "bus_stop_ser.hpp"
 
 int main() {
     bus_stop *bs1 = new bus_stop(gps(24.253475), gps(120.598994));
@@ -13,3 +15,28 @@ int main() {
 
     return 0;
 }
+#endif
+
+#ifdef PROTOBUF_EXAMPLE
+#include "addressbook.pb.h"
+#include <google/protobuf/text_format.h>
+
+int main() {
+    bus_stop *bs1 = new bus_stop(gps(24.253475), gps(120.598994));
+    
+    data::Gps *gps;
+    data::BusStop bus_stop;
+    gps = bus_stop.mutable_latitude();
+    gps->set_degrees(bs1->latitude.degrees);
+    gps = bus_stop.mutable_longitude();
+    gps->set_degrees(bs1->longitude.degrees);
+    
+    std::string str;
+    google::protobuf::TextFormat::PrintToString(bus_stop , &str);
+    std::cout << str << std::endl;
+
+    google::protobuf::ShutdownProtobufLibrary();
+
+    return 0;
+}
+#endif
