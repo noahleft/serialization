9bd55c00368e5488a3e679dc302b5506d705390a
ShihFengLin <noahleft@gmail.com>
2021-07-24 23:47:53 +0800

    compare deserialized data with original data

--- main.cpp
+++ main.cpp
@@ -7,13 +7,17 @@ int main() {
 
 #if defined(BOOST_EXAMPLE)
     buf = serialize_boost(root);
+    auto des = deserialize_boost(buf);
+    std::cout << is_equal(root, des) << std::endl;
 #elif defined(CODEGEN_EXAMPLE)
     buf = serialize_codegen(root);
+    std::cout << buf << std::endl;
 #elif defined(PROTOBUF_EXAMPLE)
     buf = serialize_protobuf(root);
+    std::cout << buf << std::endl;
 #else
     buf = dump_object(root);
-#endif
     std::cout << buf << std::endl;
+#endif
     return 0;
 }
